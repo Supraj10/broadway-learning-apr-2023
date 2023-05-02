28887080e5363757147093e5c3bf939a6170a57c
Supraj10 <suprajghimire10@gmail.com>
2023-05-02 07:52:53 +0545

    git branch commands

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-s = {1, 2, 5, 9, 20}
+##git branch
+
+#1. git branch #lists all the branches and highlights the current branch
+#2. git branch <branch_name> #creates a new branch
+#3. git checkout <branch_name> #jumps (checkout) to new branch
+
+#2 and #3 can be obtained from single command from the below command:
+#git checkout -b <branch_name>
